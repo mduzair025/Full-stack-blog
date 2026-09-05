8bb6ff464458c70b27c6322ed7df393437eeb2b8
Md Uzair <mduzair025@gmail.com>
2026-09-05 09:48:44 +0000

    update

--- resources/js/components/pages/Posts.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Posts() {
-    return <div>Posts page</div>;
-}
-
-export default Posts;
--- resources/js/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import postsReducer from "../reducers/postsReducer";
-import adminAuthReducer from "../reducers/admin/authReducer";
-
-export default combineReducers({
-    posts: postsReducer,
-    adminAuth: adminAuthReducer
-});
